3fddfe190ad2ccbfd8efdd2aeff718a31e4742a5
Brian Burch <Bburch05@gmail.com>
2018-06-28 21:32:20 -0400

    html setup

---
+++ assets/java/game.js
@@ -0,0 +1,13 @@
+
+var config = {
+    apiKey: "AIzaSyD2dxUfttvFU6ZF8ORVFYELFrKalcUtO5c",
+    authDomain: "rpsmulti-50281.firebaseapp.com",
+    databaseURL: "https://rpsmulti-50281.firebaseio.com",
+    projectId: "rpsmulti-50281",
+    storageBucket: "rpsmulti-50281.appspot.com",
+    messagingSenderId: "886910899223"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
